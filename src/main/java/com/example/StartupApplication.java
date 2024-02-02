dba32cfa35b65c581b4ccf698ca48935e31288d9
Tomoki TSUDA <138654305+tsuda-tomoki@users.noreply.github.com>
2024-02-02 20:45:49 +0900

    Spring Application に java doc を追加

--- src/main/java/com/example/StartupApplication.java
+++ src/main/java/com/example/StartupApplication.java
@@ -3,11 +3,18 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * アプリケーションを起動するクラス.
+ */
 @SpringBootApplication
 public class StartupApplication {
 
+  /**
+   * 起動するための main メソッド.
+   *
+   * @param args 使用しない.
+   */
   public static void main(String[] args) {
     SpringApplication.run(StartupApplication.class, args);
   }
-
 }
