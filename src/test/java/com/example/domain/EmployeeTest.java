bcbb17f3ebd5bd1a81e445d4a16749c9e1ec90dd
Tomoki TSUDA <138654305+tsuda-tomoki@users.noreply.github.com>
2024-02-03 16:00:15 +0900

    EmployeeTestの実装、pomの編集

---
+++ src/test/java/com/example/domain/EmployeeTest.java
@@ -0,0 +1,51 @@
+package com.example.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class EmployeeTest {
+
+  @ParameterizedTest
+  @CsvSource({
+      "aaa, Taro, Yamada",
+      " , Taro, Yamada",
+      "'', Taro, Yamada",
+      "0, Taro, Yamada"
+  })
+  void 従業員IDがガード条件に違反する場合(String id, String firstName, String lastName) throws Exception {
+    // assert
+    assertThatThrownBy(() -> new Employee(id, firstName, lastName))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessageContaining("従業員 ID が不正です。");
+  }
+
+  @ParameterizedTest
+  @CsvSource({
+      "1, 1111, Yamada",
+      "1, , Yamada",
+      "1, '', Yamada",
+      "1, null, Yamada"
+  })
+  void 従業員の名前がガード条件に違反する場合(String id, String firstName, String lastName) throws Exception {
+    // assert
+    assertThatThrownBy(() -> new Employee(id, firstName, lastName))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessageContaining("従業員の名前が不正です。");
+  }
+
+  @ParameterizedTest
+  @CsvSource({
+      "1, Taro, 111111",
+      "1, Taro, ",
+      "1, Taro, ''",
+      "1, Taro, null"
+  })
+  void 従業員の名字がガード条件に違反する場合(String id, String firstName, String lastName) throws Exception {
+    // assert
+    assertThatThrownBy(() -> new Employee(id, firstName, lastName))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessageContaining("従業員の名前が不正です。");
+  }
+}
