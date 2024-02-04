0a0e5109554ca98a7d97ddf6cb7a05617b5dd865
Tomoki TSUDA <138654305+tsuda-tomoki@users.noreply.github.com>
2024-02-04 13:27:11 +0900

    EmployeeRepositoryImplのテストを実装

---
+++ src/test/java/com/example/infrastructure/EmployeeRepositoryImplTest.java
@@ -0,0 +1,45 @@
+package com.example.infrastructure;
+
+import com.example.domain.Employee;
+import com.example.domain.EmployeeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+class EmployeeRepositoryImplTest {
+
+  @Mock
+  private EmployeeMapper employeeMapper;
+
+  @InjectMocks
+  private EmployeeRepository employeeRepository = new EmployeeRepositoryImpl();
+
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @Test
+  void 全件検索ができる場合() {
+    // setup
+    List<Employee> expected = List.of(
+        new Employee("1", "Taro", "Yamada"),
+        new Employee("2", "Jiro", "Yamada")
+    );
+
+    when(employeeMapper.findAll()).thenReturn(expected);
+
+    // execute
+    List<Employee> actual = employeeRepository.findByAllEmployees();
+
+    // assert
+    assertThat(actual).isEqualTo(expected);
+  }
+}
