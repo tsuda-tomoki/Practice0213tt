bcbb0c0774b82b5357c7e0406dc4adb6ba50d119
Tomoki TSUDA <138654305+tsuda-tomoki@users.noreply.github.com>
2024-02-10 14:34:00 +0900

    MapperのテストにPOSTを追加

--- src/main/java/com/example/infrastructure/mapper/EmployeeMapper.java
+++ src/main/java/com/example/infrastructure/mapper/EmployeeMapper.java
@@ -1,8 +1,11 @@
 package com.example.infrastructure.mapper;
 
 import com.example.domain.entity.Employee;
+import com.example.presentation.request.PostEmployeeRequest;
 import java.util.List;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 
 /**
@@ -16,4 +19,8 @@ public interface EmployeeMapper {
 
   @Select("SELECT id, first_name, last_name FROM employees WHERE id = #{id}")
   Employee findById(String id);
+
+  @Insert("INSERT INTO employees (id, first_name, last_name) VALUES (nextval('EMPLOYEE_ID_SEQ'), #{firstName}, #{lastName})")
+  Integer insert(Employee employee);
+
 }
