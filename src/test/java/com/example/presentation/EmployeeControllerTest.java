23a5b4b016d1f5a93cef4a7a6c380b2f69415be6
Tomoki TSUDA <138654305+tsuda-tomoki@users.noreply.github.com>
2024-02-10 10:21:03 +0900

    コントローラーテストの修正

--- src/test/java/com/example/presentation/EmployeeControllerTest.java
+++ src/test/java/com/example/presentation/EmployeeControllerTest.java
@@ -1,30 +1,23 @@
 package com.example.presentation;
 
 import static com.example.TestUtils.readFrom;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.doReturn;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.example.domain.AllEmployees;
 import com.example.domain.Employee;
 import com.example.domain.EmployeeService;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.assertj.core.api.Assertions.*;
 
 
 @SpringBootTest
@@ -34,11 +27,8 @@ class EmployeeControllerTest {
   @Autowired
   private MockMvc mockMvc;
 
-  @Mock
+  @MockBean
   private EmployeeService employeeService;
-
-  @InjectMocks
-  private EmployeeController employeeController;
 
   @BeforeEach
   void setUp() {
@@ -53,7 +43,7 @@ class EmployeeControllerTest {
         new Employee("2", "Jiro", "Yamada")
         );
 
-    when(employeeService.findByAllEmployeesOfService()).thenReturn(expected);
+    doReturn(expected).when(employeeService).findByAllEmployeesOfService();
 
     mockMvc.perform(get("/v1/employees"))
         .andExpect(status().isOk())
