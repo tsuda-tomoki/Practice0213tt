9d99e4fef1180bcbfb6f98cdd27759c8cec4e20c
Tomoki TSUDA <138654305+tsuda-tomoki@users.noreply.github.com>
2024-02-10 11:42:57 +0900

    コントローラークラスにID検索を実装

--- src/main/java/com/example/presentation/EmployeeController.java
+++ src/main/java/com/example/presentation/EmployeeController.java
@@ -1,10 +1,12 @@
 package com.example.presentation;
 
+import com.example.domain.entity.Employee;
 import com.example.domain.service.EmployeeService;
 import com.example.presentation.response.AllEmployeesResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +22,12 @@ public class EmployeeController {
   @GetMapping("/v1/employees")
   @ResponseStatus(HttpStatus.OK)
   public AllEmployeesResponse findByAllEmployees() {
-    System.out.println(employeeService.findByAllEmployeesOfService());
     return new AllEmployeesResponse(employeeService.findByAllEmployeesOfService());
+  }
+
+  @GetMapping("/v1/employees/{id}")
+  @ResponseStatus(HttpStatus.OK)
+  public Employee findByEmployeeId(@PathVariable String id) {
+    return employeeService.findByEmployeeIdOfService(id);
   }
 }
