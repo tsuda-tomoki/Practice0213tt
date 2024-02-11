24c4395925f3c1cf8a9d506714802448aae5fb57
Tomoki TSUDA <138654305+tsuda-tomoki@users.noreply.github.com>
2024-02-11 15:20:44 +0900

    リポジトリのテストに更新を追加

--- src/test/java/com/example/infrastructure/EmployeeRepositoryImplTest.java
+++ src/test/java/com/example/infrastructure/EmployeeRepositoryImplTest.java
@@ -3,6 +3,7 @@ package com.example.infrastructure;
 import com.example.domain.entity.Employee;
 import com.example.infrastructure.mapper.EmployeeMapper;
 import com.example.infrastructure.repository.EmployeeRepositoryImpl;
+import com.example.presentation.request.UpdateEmployeeRequest;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -81,8 +82,19 @@ class EmployeeRepositoryImplTest {
     // setup
     when(employeeMapper.delete("1")).thenReturn(1);
 
-    // execute
+    // execute & assert
     assertThatCode(() -> employeeRepository.deleteByEmployeeOfRepository("1"))
+        .doesNotThrowAnyException();
+  }
+
+  @Test
+  void 更新ができる場合() {
+    // setup
+    UpdateEmployeeRequest updateEmployeeRequest = new UpdateEmployeeRequest("Taro", "Yama");
+    when(employeeMapper.update("1", updateEmployeeRequest)).thenReturn(1);
+
+    // execute & assert
+    assertThatCode(() -> employeeRepository.updateByEmployeeOfRepository("1", updateEmployeeRequest))
         .doesNotThrowAnyException();
   }
 }
